968396cd4fb87ddf25f21d6e9a5801bcd1f7aee3
HimanshuMotiani <himanshu.motiani93@gmail.com>
2021-11-15 17:06:49 +0530

    css-images-add-html

---
+++ block-BNaabo/server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express()
+app.use((req,res,next)=>{
+    console.log(req.method,req.url);
+    next();
+})
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(__dirname + "/public/images"))
+app.post("/json",(req,res)=>{
+    console.log(req.body);
+})
+app.post("/contact",(req,res)=>{
+    console.log(req.body);
+})
+app.get("/",(req,res)=>{
+res.sendFile(__dirname + "/index.html")
+})
+app.listen(4000,()=>{})
